978341c25a9d89aca69aa75cda8aa37caab1e48b
Dmitry Afonchenko <d.afonchenko@dodopizza.com>
2022-11-14 23:34:30 +0400

    Add two comparable versions

--- AsyncEnumerableVsTaskEnumerable/Benchmarks.cs
+++ AsyncEnumerableVsTaskEnumerable/Benchmarks.cs
@@ -17,5 +17,25 @@ namespace AsyncEnumerableVsTaskEnumerable
         {
             // Implement your benchmark here
         }
+
+        // Comparable versions
+        // private async Task<List<T>> ReadAllAsync<T>(Func<DataReader, T> action, CancellationToken cancellationToken)
+        // {
+        //     var result = new List<T>();
+        //     while (await _reader.ReadAsync(cancellationToken))
+        //     {
+        //         result.Add(action(new DataReader(_reader)));
+        //     }
+        //
+        //     return result;
+        // }
+		      //
+        // private async IAsyncEnumerable<T> ReadAllAsyncEnumerable<T>(Func<DataReader, T> action, [EnumeratorCancellation] CancellationToken cancellationToken)
+        // {
+        //     while (await _reader.ReadAsync(cancellationToken))
+        //     {
+        //         yield return action(new DataReader(_reader));
+        //     }
+        // }
     }
 }
