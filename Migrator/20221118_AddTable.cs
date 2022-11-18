55ef1334cdffd7598de1d11d9822c78ebdb6348d
Dmitry Afonchenko <d.afonchenko@dodopizza.com>
2022-11-18 13:55:45 +0400

    Add migration for test table

---
+++ Migrator/20221118_AddTable.cs
@@ -0,0 +1,19 @@
+using FluentMigrator;
+
+namespace Migrator;
+
+[Migration(20221118121800)]
+public class AddTable : Migration
+{
+    public override void Up()
+    {
+        Create.Table("Log")
+            .WithColumn("Id").AsInt64().PrimaryKey().Identity()
+            .WithColumn("Text").AsString();
+    }
+
+    public override void Down()
+    {
+        Delete.Table("Log");
+    }
+}
---
+++ Migrator/Program.cs
@@ -0,0 +1,26 @@
+﻿using FluentMigrator.Runner;
+using Microsoft.Extensions.DependencyInjection;
+using Migrator;
+
+var serviceProvider = CreateServices();
+
+using var scope = serviceProvider.CreateScope();
+UpdateDatabase(scope.ServiceProvider);
+
+static IServiceProvider CreateServices()
+{
+    return new ServiceCollection()
+        .AddFluentMigratorCore()
+        .ConfigureRunner(rb => rb
+            .AddMySql5()
+            .WithGlobalConnectionString("Server=127.0.0.1;Database=log;Uid=app;Password=12345;SSL Mode=None;Allow User Variables=True;Port=3307;")
+            .ScanIn(typeof(AddTable).Assembly).For.Migrations())
+        .AddLogging(lb => lb.AddFluentMigratorConsole())
+        .BuildServiceProvider(false);
+}
+
+static void UpdateDatabase(IServiceProvider serviceProvider)
+{
+    var runner = serviceProvider.GetRequiredService<IMigrationRunner>();
+    runner.MigrateUp();
+}
